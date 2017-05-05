68f410f15d9346ef0e5965886038f6c08bcb7e9d
administrator <alisasotsenko@gmail.com>
2017-05-05 09:57:18 +0200

    TODO hints for gamemap view

--- client/src/app/gamemap/gamemap.component.ts
+++ client/src/app/gamemap/gamemap.component.ts
@@ -31,15 +31,27 @@ export class GamemapComponent implements OnInit {
 
     });
 
+    //TODO: show the map + leader/nurse on it with the leader/team name as it was shown on the paper prototype
+    // show notification waiting for others to be log in
+    // make the gamemap view as in the paper prototype for leader
+    // make the gamemap view for nurses: visualize the map, users on the map, header is same as in leader gamemapview
+
+
     this._chatService.connect();
 
     this._chatService.getMessages().subscribe((message)=>{
        console.log("message");
 
        if(message['topic'] === "game-start"){
+         // TODO: remove the notification waiting for others to be log in
+         // TODO: show notificaiton game started!
          console.log("GAME START");
          this.game = message['data'];
          console.log(this.game);
+         //TODO:Start timer
+         //show for leader availbale teams take this data from this.game object
+
+
        }
 
     });
